74f84d3188de195b05e0ffb188b17409e67894d7
Keshav Mishra <keshav0774@gmail.com>
2026-03-07 01:58:16 +0530

    fix: vite build config

--- LeetCode-Frontend/vite.config.js
+++ LeetCode-Frontend/vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react()],
+  build: {
+    minify: 'esbuild',
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          redux: ['@reduxjs/toolkit', 'react-redux'],
+          monaco: ['@monaco-editor/react'],
+        }
+      }
+    }
+  }
 })
